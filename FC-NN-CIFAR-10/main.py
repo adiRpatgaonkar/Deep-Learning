f4f46a5a15cfa96c0684cfc23550d3c4bb246369
Aditya R. Patgaonkar <adityapatgaonkar9@gmail.com>
2018-03-12 00:46:45 +0530

    Bug fixes & more files. Duh. Loading model to be fixed.

--- FC-NN-CIFAR-10/main.py
+++ FC-NN-CIFAR-10/main.py
@@ -1,12 +1,17 @@
 import do_stuff as do
+import nnCustom as nnc
 import fitting_net
 import train_net
 import test_net
 import infer
+import create
 
 def main():
 
     do.parse_arg()
+
+    if do.args.LOAD:
+        model = nnc.load_model(do.args.LOAD)
 
     if do.args.FIT:
         fitting_net.fit()
@@ -18,6 +23,8 @@ def main():
         test_net.test(model)
 
     if do.args.INFER:
+        if not do.args.TRAIN:
+            model = create.create_model()
         infer.inferences(model)
 
     print('\n' + '-' * 7 + '\nExiting\n' + '-' * 7)
