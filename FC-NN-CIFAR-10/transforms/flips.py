e92ef5d45dd1327894d5f397f25a9cb4eedb5cbf
= <=>
2018-03-19 16:03:56 +0530

    Added flip.py to data augment

---
+++ FC-NN-CIFAR-10/transforms/flips.py
@@ -0,0 +1,18 @@
+""" 
+Flip the given data (training)
+Return appended (augmented dataset)
+"""
+
+import torch
+import numpy as np
+
+def flip(image):
+    pass
+
+
+
+def augment_flipped(data):    
+    pass
+
+
+
