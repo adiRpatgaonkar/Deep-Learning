37ae8251df517ab935922b9e68413bc09e62575f
= <=>
2018-03-21 17:12:02 +0530

    More fine tuning. Added flip upside down augmentation.

--- FC-NN-CIFAR-10/tools/train_net.py
+++ FC-NN-CIFAR-10/tools/train_net.py
@@ -27,14 +27,22 @@ def train(model=None):
         model = create.create_model()
     
     print("\n+++++     TRAINING     +++++\n")
+
     model.show_log(arch=True, train=True)
+
     # Get data
     train_dataset = dset.CIFAR10(directory='data', download=True, 
                     train=True)
+
     # Data augmentation
-    train_dataset = TransformData(train_dataset, transform='flip')
-    train_dataset = TransformData(train_dataset, transform='crop')
+    # Horizhontal flips. Giving the best results
+    train_dataset = TransformData(train_dataset, transform='flipLR')
+    # Upside-down flips. Average results
+    # train_dataset = TransformData(train_dataset, transform='flipUD')
+    # Crops. Bad results
+    # train_dataset = TransformData(train_dataset, transform='crop')
     print("Training set size:", len(train_dataset.data), "images.")
+
     # Optimizer
     optimizer = nnc.Optimize(model)
     print("\n# Stochastic gradient descent #")
@@ -44,9 +52,11 @@ def train(model=None):
         # Prepare batches from whole dataset
         train_loader = dset.data_loader(train_dataset.data, 
                        batch_size=dset.CIFAR10.batch_size, shuffled=True)
+        # Iterate over batches
         for images, labels in train_loader:
             if using_gpu():
                 images = images.cuda()
+            # Training round
             model.train(images, labels)
             # Clear cache if using GPU (Unsure of effectiveness)
             if using_gpu():
--- FC-NN-CIFAR-10/vision/transforms.py
+++ FC-NN-CIFAR-10/vision/transforms.py
@@ -14,17 +14,34 @@ class TransformData():
 
     def __init__(self, orig_dataset, transform):
         self.transform = transform
-        if transform == 'flip':
-            self.data = self.flip(orig_dataset)
+        if transform == 'flipLR':
+            self.data = self.flipLR(orig_dataset)
+        if transform == 'flipUD':
+            self.data = self.flipUD(orig_dataset)
         if transform == 'crop':
             self.data = self.crop(orig_dataset)
 
-    def flip(self, orig_dataset):
+    def flipLR(self, orig_dataset):
+        """ Flips horizontally """
         data_aug = copy.deepcopy(orig_dataset)
         data_aug.data = orig_dataset.data[:]
-        print("Horizontally flipping training examples ...", end = " ")
+        print("Flipping training examples horizontally ...", end = " ")
         for image, label in orig_dataset.data:
             t_image = np.flip(image.numpy(), 2)  
+            t_image = torch.from_numpy(t_image.copy()).type(torch.FloatTensor)
+            data_aug.data.append((t_image, label))
+        print("done.")
+        return data_aug.data
+
+    def flipUD(self, orig_dataset):
+        """ Flips upside-down """
+        data_aug = copy.deepcopy(orig_dataset)
+        data_aug.data = orig_dataset.data[:]
+        print("Flipping training examples upside down ...", end = " ")
+        for image, label in orig_dataset.data:
+            # For 3 channels np.fliplr works as 
+            # putting the image as upside down
+            t_image = np.fliplr(image.numpy())  
             t_image = torch.from_numpy(t_image.copy()).type(torch.FloatTensor)
             data_aug.data.append((t_image, label))
         print("done.")
