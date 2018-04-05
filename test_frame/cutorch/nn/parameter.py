24cd54f7ae2869e91a7b7b8b6858cfc134a85a9e
= <=>
2018-04-05 16:19:22 +0530

    Spl func modification

--- test_frame/cutorch/nn/parameter.py
+++ test_frame/cutorch/nn/parameter.py
@@ -4,7 +4,18 @@
 class Parameter:
 
     def __init__(self, **kwargs):
-    	print type(kwargs)
+
+    	print kwargs
+    	if 'weight' in kwargs:
+    		module.weight = torch.randn(self.in_features,
+                                        self.out_features)
+        	module.weight.data *= 0.01 # Conditionalize this
+			module._parameters[module] = [module.weight]
+
+        if 'bias' in kwargs:
+            module.bias = torch.Tensor(1, out_features).fill_(0)
+            module._parameters[module].append(module.bias)
+        
         for key, value in kwargs.items():
             self.tag = key
             self.data = value
