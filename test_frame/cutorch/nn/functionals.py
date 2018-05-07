Fixed a dangerous bug in conv2d's o/p vol. Maxpool2d fprop rewritten. Backprop of Maxpool done.

test_frame/cutorch/nn/functionals.py
@@ -31,7 +31,7 @@ def flatten(data):
 #                                               #
 #################################################
 
-def im2col(batch, kernel_size, stride, task="conv"):
+def im2col(batch, kernel_size, stride):
     """ im2col for batch inputs i.e. 4D Tensors """
     assert batch.dim() == 4, "Input batch should be 4D tensor"
     batch_i2c = torch.Tensor()
@@ -45,12 +45,7 @@ def im2col(batch, kernel_size, stride, task="conv"):
                 im_col = image[:, i:fh, j:fw]
                 im_col = im_col.contiguous()  # tensor must be contiguous to flatten it
                 im_col.unsqueeze_(0)  # Stretch to 4D tensor
-                if task == "conv":
-                    # Flatten across 3D space
-                    im_col = im_col.view(im_col.size(0), -1)
-                elif task == "pool":
-                    # Flatten across 2D i.e. preserve depth dim
-                    im_col = im_col.view(im_col.size(1), -1)
+                im_col = im_col.view(im_col.size(0), -1)
                 i2c_per_im = torch.cat((i2c_per_im, im_col.t()), 1)  # Cat as col vector
                 fw += stride
             fh += stride
@@ -63,10 +58,10 @@ def im2col(batch, kernel_size, stride, task="conv"):
     return batch_i2c
 
 
-def col2im(input, x_size, k_size, stride, task="conv"):
+def col2im(input, x_size, k_size, stride):
     """ col2im or de-im2col for 2D tensors """
-    assert input.dim() == 2, "Input should be a 2D tensor"
-    assert len(x_size) == 4, "Input size should have 4 values for 4D tensor"
+    assert input.dim() == 2, "Input should be a 2D tensor."
+    assert len(x_size) == 4, "Input size should have 4 values for 4D tensor."
     batch_c2i = torch.Tensor()
     N, C, H, W = x_size
     batch = torch.chunk(input, N, dim=1)
@@ -75,12 +70,12 @@ def col2im(input, x_size, k_size, stride, task="conv"):
     fh = fw = k_size
     for image in batch:
         m = 0  # Number of locations convolved(mth col)
+
         c2i_per_im = torch.zeros(C, H, W)
         for i in range(0, H - k_size + 1, stride):
             for j in range(0, W - k_size + 1, stride):
                 col = image[:, m].contiguous()
-                if task == "conv":
-                    c2i_per_im[:, i:fh, j:fw] = col.view(C, k_size, k_size)
+                c2i_per_im[:, i:fh, j:fw] = col.view(C, k_size, k_size)
                 m += 1  #
                 fw += stride
             fh += stride
@@ -354,4 +349,15 @@ def grad_conv2d(cache, weight, grad_out):
     grad_Xcol = torch.mm(weight_reshaped.t(), cache)
     # Clean
     del cache, weight_reshaped, grad_out
+    return grad_Xcol
+
+
+def grad_maxpool2d(xcol_size, max_track, grad_out):
+    rows, cols = xcol_size
+    grad_Xcol = torch.Tensor(rows, cols).fill_(0)
+    grad_out = grad_out.permute(2, 3, 0, 1).contiguous()
+    grad_out = grad_out.view(-1)
+    grad_Xcol[max_track, range(max_track.size(0))] = grad_out
+    # Clean
+    del grad_out
     return grad_Xcol

test_frame/cutorch/nn/modules/conv.py
@@ -24,11 +24,11 @@ class Conv2d(Module):
         assert pad >= 0, ("Invalid padding value. Should be >= 0")
         assert stride > 0, ("Invalid stride. Should be > 0")
         self.idx = -1
+        self.channels = channels
         self.kernel_size = kernel_size
         self.kernels, self.padding, self.stride = kernels, pad, stride
         self.input = None  # TODO:CLEAN
         self.data = 0  # TODO:CLEAN
-        self.height = self.width = 0
         self.output_dim = [0, 0, 0]
         self.batch_ims = None # im2col data.  # TODO:CLEAN
         # Parameters' creation
@@ -64,19 +64,25 @@ class Conv2d(Module):
         assert self.input.dim() in (3, 4), ("Input tensor should be 3D or 4D")
         if self.input.dim() == 3: # Convert to 4D tensor
             self.input = torch.unsqueeze(self.input, 0)
-        self.batches, self.channels, self.height, self.width = self.input.size()
+        self.N, self.C, self.H, self.W = self.input.size()
+        assert self.C == self.channels, \
+               "Input channels should be {}. Got {} channels".format(self.channels, self.C)
         self.output_dim[0] = self.kernels
-        self.output_dim[1] = ((self.width - self.kernel_size + 2 * self.padding) / self.stride + 1)
-        self.output_dim[2] = ((self.height - self.kernel_size + 2 * self.padding) / self.stride + 1)
+        self.output_dim[1] = ((self.H - self.kernel_size + 2 * self.padding) / self.stride + 1)
+        self.output_dim[2] = ((self.W - self.kernel_size + 2 * self.padding) / self.stride + 1)
 
     def prepare_input(self):
-        """ Prepare in features """
+        """ 
+        Prepare in features
+        1. Padding
+        2. im2col
+        """
         # 1. Padding: self.input should be 4D
         if self.padding > 0:
             self.input = F.pad_image(self.input, self.padding)
             if type(self.input) is np.ndarray: # If numpy array, convert to tensor before conv op.
                 self.input = torch.from_numpy(self.input)
-        return F.im2col(self.input, self.kernel_size, self.stride, task="conv")
+        return F.im2col(self.input, self.kernel_size, self.stride)
  
     def forward(self, in_features):
         """ Convolution op (Auto-correlation i.e. no kernel flipping) """
@@ -87,13 +93,12 @@ class Conv2d(Module):
             self.input = in_features 
         self.create_output_vol()
         print("Input to conv layer:", self.input.size())
-        N = self.input.size(0) 
         self.input = self.prepare_input() # im2col'ed input
-        print("Post im2col:", self.input.size())
+        #print("Post im2col:", self.input.size())
         self.data = F.conv_2d(self.input, self.weight.data.view(self.weight.data.size(0), -1), 
                               self.bias.data.view(self.bias.data.size(0), -1))
         # Reshape to the o/p feature volume
-        self.data = self.data.view(N, self.output_dim[0], self.output_dim[1], 
+        self.data = self.data.view(self.N, self.output_dim[0], self.output_dim[1], 
                                    self.output_dim[2])
         # print("Reshaped:", self.data.size())
         # Clean
@@ -119,9 +124,8 @@ class Conv2d(Module):
         else:
             self.grad['input'] = F.grad_conv2d(cache, self.weight.data, gradients['input']) 
         # De-im2col grad ins i.e. F.col2im(self.grad['input'])
-            self.grad['input'] = F.col2im(self.grad['input'], (self.batches, self.channels,
-                                                               self.height, self.width),
-                                          self.kernel_size, self.stride, task="conv")
+            self.grad['input'] = F.col2im(self.grad['input'], (self.N, self.C, self.H, self.W),
+                                          self.kernel_size, self.stride)
         # Clean
         del gradients, cache
         return self.grad

test_frame/cutorch/nn/modules/pooling.py
@@ -31,7 +31,7 @@ class MaxPool2d(Module):
             self.stride = stride
         self.input = None  # TODO:CLEAN
         self.data = 0  # TODO:CLEAN
-        self.height = self.width = 0
+        self.N = self.H = self.W = 0
         self.output_dim = [0, 0, 0]  # For a single image
         self.batch_ims = None # im2col data.  # TODO:CLEAN
         # Gradients' creation
@@ -45,15 +45,16 @@ class MaxPool2d(Module):
         assert self.input.dim() in (3, 4), ("Input tensor should be 3D or 4D")
         if self.input.dim() == 3: # Convert to 4D tensor
             self.input = torch.unsqueeze(self.input, 0)
-        self.height, self.width = self.input.size()[2:]
+        self.N, self.C, self.H, self.W = self.input.size()
         self.output_dim[0] = self.input.size(1)
-        self.output_dim[1] = ((self.width - self.kernel_size) / self.stride) + 1
-        self.output_dim[2] = ((self.height - self.kernel_size) / self.stride) + 1
+        self.output_dim[1] = ((self.H - self.kernel_size) / self.stride) + 1
+        self.output_dim[2] = ((self.W - self.kernel_size) / self.stride) + 1
 
     def prepare_input(self):
         """ Prepare in features """
-        # 1. im2col operation (Batch op) 
-        return F.im2col(self.input, self.kernel_size, self.stride, task="pool") 
+        # 1. im2col operation (Batch op) Different than conv's im2col
+        self.input_reshaped = self.input.view(self.N * self.C, 1, self.H, self.W)
+        return F.im2col(self.input_reshaped, self.kernel_size, self.stride) 
 
     def forward(self, in_features):
         """ Pooling op """
@@ -71,10 +72,17 @@ class MaxPool2d(Module):
         print("Post_max_pool:", self.data.size()) 
         self.data = self.data.view(N, self.output_dim[0], 
                                    self.output_dim[1], self.output_dim[2])
-        # print("Reshaped:", self.data.size())
+        print("Reshaped:", self.data.size())
         # Clean
         del in_features
         return self
 
-    def backward(self):
-        pass
+    def backward(self, gradients):
+        self.grad['input'] = F.grad_maxpool2d(self.input.size(), self.max_track, gradients['input'])
+        print(self.grad['input'].size())
+        self.grad['input'] = F.col2im(self.grad['input'], (self.N * self.C, 1, self.H, self.W),
+                                      self.kernel_size, self.stride)
+         # Different than conv's col2im
+        self.grad['input'] = self.grad['input'].view(self.N, self.C, self.H, self.W)
+        return self.grad
+                                    

test_frame/main_test.py
@@ -4,16 +4,10 @@ import torch
 
 import cutorch.nn as nn
 
-#from evaluate import *
-
 # if cutorch.gpu_check.available():
 #     using_gpu = True
 # else:
 #     using_gpu = False
-
-# Global vars
-global images, ground_truths, outputs, predicted, loss
-global train_loader, test_loader
 
 # Hyperparameters
 max_epochs = 10
@@ -68,6 +62,7 @@ class CNN(nn.Module):
 
 image = (torch.LongTensor(2, 3, 32, 32).random_(0, 255)).float()
 cnn = CNN()
+global outputs
 outputs = cnn(image)
 print("Net-out:", outputs.data.size())
 print(cnn.layer1[1].parameters())
@@ -75,6 +70,7 @@ print(cnn.layer1[1].parameters())
 # Dummy grads
 from collections import OrderedDict as OD
 gradients = OD()
-gradients['input'] = torch.randn(2, 16, 28, 28)
-grad = cnn.layer1[1].backward(gradients)
-cnn.layer1[0].backward(grad)
+gradients['input'] = torch.randn(2, 16, 14, 14)
+grad = cnn.layer1[3].backward(gradients)
+grad = cnn.layer1[1].backward(grad)
+grad = cnn.layer1[0].backward(grad)