Bias grad reshape bug fixed. Backprop nearly done. This is the debug train code. Remember

test_frame/cutorch/nn/functionals.py
@@ -335,7 +335,7 @@ def grad_conv2d_bias(grad_out):
         grad_bias.append(torch.sum(grad_out[:, c, :, :]))
     # Clean
     del grad_out
-    return torch.Tensor(grad_bias)
+    return torch.Tensor(grad_bias).unsqueeze(1)
 
 
 def grad_conv2d_weight(input, grad_out):
@@ -360,8 +360,11 @@ def grad_conv2d(cache, weight, grad_out):
 def grad_maxpool2d(xcol_size, max_track, grad_out):
     rows, cols = xcol_size
     grad_Xcol = torch.Tensor(rows, cols).fill_(0)
-    grad_out = grad_out.permute(2, 3, 0, 1).contiguous()
-    grad_out = grad_out.view(-1)
+    if grad_out.dim() == 2:
+        grad_out = grad_out.t().contiguous().view(-1)
+    elif grad_out.dim() == 4:
+        grad_out = grad_out.permute(2, 3, 0, 1).contiguous()
+        grad_out = grad_out.view(-1)
     grad_Xcol[max_track, range(max_track.size(0))] = grad_out
     # Clean
     del grad_out

test_frame/main_test.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
 
-import torch
-
+import cutorch
 import cutorch.nn as nn
+import cutorch.nn.functionals as F
+import cutorchvision.datasets as dsets
+from cutorchvision.transforms import Transforms
 
 # if cutorch.gpu_check.available():
 #     using_gpu = True
@@ -11,28 +13,29 @@ import cutorch.nn as nn
 
 # Hyperparameters
 max_epochs = 10
-learning_rate = 5e-2
-lr_decay = 5e-5
+learning_rate, lr_decay = 5e-2, 5e-5
+reg = 1e-3
+batch_size = 100
 
 # Get training data for training and Cross validation
-# trainset = dsets.CIFAR10(dir="cutorchvision/data",
-#                          download=True, train=True,
-#                          form="tensor")
-# # Data augmentation
-# train_dataset = Transforms(dataset=train_dataset,
-#                            lr_flip=True, crop=False)
+trainset = dsets.CIFAR10(dir="cutorchvision/data",
+                         download=True, train=True,
+                         form="tensor")
+# Data augmentation
+trainset = Transforms(dataset=trainset,
+                      lr_flip=True, crop=False)
 # For testing
-# test_dataset = dsets.CIFAR10(directory="cutorchvision/data",
-#                              download=True, test=True,
-#                              form="tensor")
-# # Testing data for validation
-# test_loader = cutorch.utils.data.DataLoader(data=test_dataset.data,
-#                                             batch_size=10000,
-#                                             shuffled=False)
+testset = dsets.CIFAR10(dir="cutorchvision/data",
+                        download=True, test=True,
+                        form="tensor")
+# Testing data for validation
+test_loader = cutorch.utils.data.DataLoader(data=testset.data,
+                                            batch_size=10000,
+                                            shuffled=False)
 
-# train_loader = cutorch.utils.data.DataLoader(data=trainset.data,
-#                                             batch_size=100,
-#                                             shuffled=True)
+train_loader = cutorch.utils.data.DataLoader(data=trainset.data,
+                                             batch_size=batch_size,
+                                             shuffled=True)
 
 
 class CNN(nn.Module):
@@ -61,9 +64,12 @@ class CNN(nn.Module):
         out = self.fc(out)
         return out
 
-
-image = (torch.LongTensor(2, 3, 32, 32).random_(0, 255)).float()
+import torch
+image = (torch.LongTensor(100, 3, 32, 32).random_(0, 255)).float()
 cnn = CNN()
+criterion = nn.CrossEntropyLoss()
+optimizer = cutorch.optim.Optimizer(cnn, lr=learning_rate, lr_decay=lr_decay, reg=reg)
+'''
 cnn.train()
 outputs = cnn(image)
 print("Net-out:", outputs.data, outputs.data.size())
@@ -72,10 +78,21 @@ print(cnn.layer1[1].parameters(), cnn.forward_path)
 # Dummy grads
 from collections import OrderedDict as OD
 gradients = OD()
-gradients['in'] = torch.randn(2, 16, 14, 14)
+gradients['in'] = torch.randn(100, 16, 14, 14)
 grad = cnn.layer1[3].backward(gradients)
 grad = cnn.layer1[2].backward(grad)
 grad = cnn.layer1[1].backward(grad)
 grad = cnn.layer1[0].backward(grad)
 cnn.eval()
 print(outputs.data)
+'''
+for epoch in range(2):
+   for i, (images, labels) in enumerate(train_loader):
+        cnn.train()
+        optimizer.zero_grad()
+        outputs = cnn(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        print ('Epoch [%d/%d], Iter[%d/%d] Loss: %.4f'
+                %(epoch + 1, 2, i + 1, len(trainset.data)//batch_size, loss.data)) 